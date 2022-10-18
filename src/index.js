aa67761fa407fbb191684bbaaf95e9704e4851f3
Amit Asher <asheramit4@gmail.com>
2022-10-18 21:36:20 +0300

    add print

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const port = 3000;
 // parse application/json
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    console.log('GET /');
+    res.send('Hello World!')
+});
 
 // start listening
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
